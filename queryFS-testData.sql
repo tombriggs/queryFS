50a5f0c76feee476fd75568044073e86c777254a
Thomas Briggs <tom@fulltablescan.com>
2021-10-24 17:03:40 -0400

    A second test query in a different directory

--- queryFS-testData.sql
+++ queryFS-testData.sql
@@ -1,7 +1,3 @@
-INSERT INTO queryFS_query(queryName, queryText) VALUES('queryFS_tables', 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ''QueryFS''');
-
-INSERT INTO queryFS_queryDirectoryMap(queryId, directoryId) VALUES(LAST_INSERT_ID(), 1);
-
 INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('a', 1);
 INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('b', 1);
 INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('c', 1);
@@ -9,4 +5,10 @@ INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('c', 1);
 INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('aa', 2);
 INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('bb', 3);
 INSERT INTO queryFS_directory(directoryName, parentDirectoryId) VALUES('cc', 4);
+
+INSERT INTO queryFS_query(queryName, queryText) VALUES('queryFS_tables', 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ''QueryFS''');
+INSERT INTO queryFS_queryDirectoryMap(queryId, directoryId) VALUES(LAST_INSERT_ID(), 1);
+
+INSERT INTO queryFS_query(queryName, queryText) VALUES('queryFS_schemas', 'SELECT * FROM INFORMATION_SCHEMA.SCHEMATA');
+INSERT INTO queryFS_queryDirectoryMap(queryId, directoryId) VALUES(LAST_INSERT_ID(), 5);
 
